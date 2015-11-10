3657c1ba6424e6b3ce4aa78203f6813144599e9f
radlbauer <rudiradlbauer@yahoo.de>
2015-11-10 15:57:04 +0100

    erste Version von MavenDemo

---
+++ MavenDemo/src/main/java/at/spengergasse/MavenDemo/App.java
@@ -0,0 +1,23 @@
+package at.spengergasse.MavenDemo;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        Logger.getLogger(App.class).info("das ist eine Logging-Message");
+    }
+    
+    
+    public static int test(int x)
+    {
+    	return x * 6;
+    }
+}
